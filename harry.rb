0e0807d1422a06c28b123d08fdb36f6ffe9705ac
Pravendra Singh <hackpravj@gmail.com>
2014-05-22 22:52:27 +0530

    stores smiley locally and serve from there

--- harry.rb
+++ harry.rb
@@ -1,6 +1,6 @@
 # Harry
 # =====
-# a tiny plugin to have :smiley: in your jekyll blogs
+# Plugin to have :smiley: in your jekyll blogs
 # it uses the famous Emoji-Cheat-Sheet : 'http://www.emoji-cheat-sheet.com/'
 #
 # https://github.com/pravj/Harry
@@ -9,25 +9,56 @@ require 'net/http'
 
 module Jekyll
   module Harry
-    def harry(text)
+    # returns an image tag, having image source as local image file
+    def ImageTag(smiley)
+      '<img src="/public/smileys/'+smiley+'.png" title=":'+smiley+':" height="20px" width="20px" style="display:inline;margin:0;vertical-align:middle"/>'
+    end
+
+    # stores an image locally, by default it store in '/public/smileys'
+    def ImageStore(smiley)
+      Net::HTTP.start('www.emoji-cheat-sheet.com') do |http|
+        res = http.get('/graphics/emojis/'+smiley+'.png')
+        File.open('public/smileys/'+smiley+'.png', 'wb') { |file| file.write(res.body) }
+      end
+    end
+
+    # checks, if a smiley actually exists or not
+    def SmileyExist(smiley)
+      uri = URI('http://www.emoji-cheat-sheet.com/graphics/emojis/'+smiley+'.png')
+      res = Net::HTTP.get_response(uri)
+
+      if res.code == '200'
+        return true
+      else
+        return false
+      end
+    end
+
+    def Harry(text)
       # matches all :smiley: style text in content
       text.to_str.gsub(/:([a-z0-9]+):/) do |match|
 
-        # image source for smiley on remote server
-        imgsrc = 'http://www.emoji-cheat-sheet.com/graphics/emojis/'+$1+'.png'
-        # image tage
-        img = '<img src="http://www.emoji-cheat-sheet.com/graphics/emojis/'+$1+'.png" title=":'+$1+':" height="20px" width="20px" style="display:inline;margin:0;vertical-align:middle"/>'
-        
-        # checks if image is available there or not
-        uri = URI(imgsrc)
-        res = Net::HTTP.get_response(uri)
+        # proposed image path, if image is locally available
+        local_image = File.join(Dir.pwd, 'public', 'smileys', $1+'.png')
 
-        # return image tag if image available
-        if res.code == '200'
-          img
-        # return :smiley: text as it is
+        # returns image tag, if image locally exists
+        if File.exist?(local_image)
+          ImageTag($1)
+
+        # image doesn't exists locally
         else
-          match
+          # stores the image and returns image tag, if smiley is valid
+          if SmileyExist($1)
+            ImageStore($1)
+            if File.exist?(File.join(Dir.pwd, 'public/smileys/'+$1+'.png'))
+              puts 'y'
+            else
+              puts 'n'
+            end
+            ImageTag($1)
+          else
+            ':'+$1+':'
+          end
         end
 
       end
